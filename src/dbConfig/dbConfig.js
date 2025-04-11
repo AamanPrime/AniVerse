4132943fd9a42a7744f7649e793935fc5f46e557
devan191 <devan23191@iiitd.ac.in>
2025-04-11 19:00:23 +0530

    Reapply "updated sqldata"
    This reverts commit d95bcf084d5dfe0a507c29f132d35abf3ff5c212.

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -3,7 +3,7 @@ import {Pool} from "pg";
 const pool = new Pool({
   user: "postgres", // Change to your PostgreSQL username
   host: "localhost",
-  database: "aniverse5", // Change to your database name
+  database: "aniverse8", // Change to your database name
   password: "admin", // Change to your password
   port: 5432, // Default PostgreSQL port
 });
